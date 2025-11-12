58a12367cc46311e17a9aec9ba74ea5e80fd5a35
perspectivas-py <prensa.perspectivas@gmail.com>
2025-11-12 13:31:44 -0300

    Create parseFrontmatter.js

---
+++ utils/parseFrontmatter.js
@@ -0,0 +1,15 @@
+export function parseFrontmatter(markdownContent) {
+  const frontmatterRegex = /^---\s*([\s\S]*?)\s*---/;
+  const match = frontmatterRegex.exec(markdownContent);
+  const data = { frontmatter: {}, content: markdownContent };
+  if (match) {
+    data.content = markdownContent.replace(match[0], '').trim();
+    match[1].split('\n').forEach(line => {
+      const [key, ...valueParts] = line.split(':');
+      if (key && valueParts.length > 0) {
+        data.frontmatter[key.trim()] = valueParts.join(':').trim().replace(/"/g, '');
+      }
+    });
+  }
+  return data;
+}
