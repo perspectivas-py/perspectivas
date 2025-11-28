4d11b7012b9415efb5f19c732aaa3a63ce0aa6bc
perspectivas-py <prensa.perspectivas@gmail.com>
2025-11-28 09:57:29 -0300

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -3,8 +3,9 @@ const { AuthorizationCode } = require('simple-oauth2');
 // /api/auth.js
 
 module.exports = (req, res) => {
-  const clientId = process.env.GITHUB_CLIENT_ID;
-  const redirectUri = process.env.REDIRECT_URI;
+  const clientId = process.env.Ov23li5ZS4FB1zXwg4Q8;
+  const redirectUri = process.env.https://perspectivaspy.vercel.app/api/callback
+;
 
   if (!clientId || !redirectUri) {
     console.error('Faltan GITHUB_CLIENT_ID o REDIRECT_URI en las env vars');
